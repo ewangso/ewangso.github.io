1c2659c88dacfce92553398ad774070c9ecbfe7a
ewangso <skyromex@gmail.com>
2020-09-25 00:25:50 -0400

    Updates

---
+++ precache-manifest.86912381beb01387a5c0b493797ab625.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e309e823872377d9bf0cae5d6e7a62e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996c3d3c238ca3cc4fba",
+    "url": "/static/css/main.19c239a2.chunk.css"
+  },
+  {
+    "revision": "70eef37a27151e257d63",
+    "url": "/static/js/2.1e67b3fc.chunk.js"
+  },
+  {
+    "revision": "55229ae03fcee1f9d8b3da7f85ea7da8",
+    "url": "/static/js/2.1e67b3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996c3d3c238ca3cc4fba",
+    "url": "/static/js/main.10f6554a.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "28fd4fb07a70f49e220624e89f963ba1",
+    "url": "/static/media/MedOntology.28fd4fb0.png"
+  },
+  {
+    "revision": "07fadc51537985f03e7468dd9597e4d2",
+    "url": "/static/media/Resume.07fadc51.pdf"
+  },
+  {
+    "revision": "14228c7a6cea369958dfb2708b4624dc",
+    "url": "/static/media/Twitter-Logo.14228c7a.png"
+  },
+  {
+    "revision": "5b168f428a275192dc3c221277a5ab9d",
+    "url": "/static/media/coding.5b168f42.jpeg"
+  },
+  {
+    "revision": "d8cd809c520896eba4cc45eca0c9c9cc",
+    "url": "/static/media/computerdesk.d8cd809c.jpg"
+  },
+  {
+    "revision": "e4700f800a509a5cb70358ce98044b85",
+    "url": "/static/media/facebook.e4700f80.png"
+  },
+  {
+    "revision": "c5c3769f8bc301a1041dcf4f26242c26",
+    "url": "/static/media/flashcards.c5c3769f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "35cd9cd4450537aa73160cb44d66bea0",
+    "url": "/static/media/map.35cd9cd4.jpg"
+  },
+  {
+    "revision": "36743b18c4b8a8c7ea1c5a3d004e1f38",
+    "url": "/static/media/njit.36743b18.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyPortfolio/precache-manifest.6289aa20c5a741d65b879a4d03131103.js"
+  "/precache-manifest.86912381beb01387a5c0b493797ab625.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
